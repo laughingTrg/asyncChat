cc7d2e0670ca3725265cccaa7b61fc419be6f7b5
laughing <laughinChrist@gmail.com>
2022-12-28 20:30:59 +0500

    lesson_6 wrappers, ready

---
+++ lesson_6/common/constants.py
@@ -0,0 +1,15 @@
+
+DEFAULT_IP = "127.0.0.1"
+DEFAULT_PORT = 7777
+MAX_MSG_BYTES = 1024
+ENCODING = "utf-8"
+MAX_CONNECTIONS = 5
+
+ACTION = "action"
+TIME = "time"
+PRESENCE = "presence"
+RESPONSE = "response"
+ERROR = "error"
+USER = "user"
+ACCOUNT_NAME = "account_name"
+ALERT = "alert"
